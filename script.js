81beb284fcbf295785159de727e0d272bf300dac
GhazaalNorouzi <ghazaalnorouzi.d@gmail.com>
2025-02-24 11:23:56 -0800

    submit js1

---
+++ script.js
@@ -0,0 +1,74 @@
+///////////////////////////////
+// 1) If the value of x is 40, then what is the output of the following program?
+// x % 10
+////your answer://///
+1)answer:0
+
+//////////////////////////////
+// 2) Which is the correct way to write a comment in JavaScript?
+// a) {#...#}
+// b) <!---.......---!>
+// c) //......
+// d) \\ .....
+
+////your answer://///
+2)answer:c
+///////////////////////////////
+// 3)Witch one of the following is correct?
+// a) i=+1
+// b) i+=1
+// c) i=i++1
+// d) +i+
+////your answer://///
+3)answer:b
+///////////////////////////////
+// 4) How do you declare a variable in javascript and write a short explanation about the
+// differences between let, const, var
+////your answer://///
+4)In JavaScript we can declare a variable using var, let, or const for example:
+var name = "ghazaal";  
+let age = 26;  
+const x = 30;
+var → Its value can be updated and re-declared.
+let → Its value can be updated, but re-declaring it in the same scope is not allowed.
+const → Its value cannot be updated or re-declared.
+
+*var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables can neither be updated nor re-declared. They are all hoisted to the top of their scope. But while var variables are initialized with undefined , let and const variables*
+
+
+
+/////////////////////////////
+// 5) What is null in javascript?
+
+////your answer://///
+5)
+
+in JavaScript, null and undefined represent the absence of a value. null is an assignment value that represents no value or no object. It is often used to indicate that a variable has no value or that an object does not exist
+
+///////////////////////////////
+// 6)declare three variables with const, let and var
+////your answer://///
+7) let myFavoritColer="pink";
+let price=850;
+const age=26;
+
+
+
+///////////////////////////////
+// 7) for assigning a value to a variable we use:
+// a) *
+// b) =✔
+// c) -
+// d) 
+
+
+///////////////////////////////
+// 8) what is the ouput of below operation?
+// 10 + 3 * 2 / 2
+=13
+////your answer://///
+
+///////////////////////////////
+// 9) Fill the empty space, with correct operation to make the equality true
+// 40 .......... 2 = 0
+// 1)/       2)% ✔        3)-      4)+
